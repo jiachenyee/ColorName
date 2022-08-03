62e36844f3b282029562671c4c2e087c30f97de6
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-08-04 01:27:04 +0800

    change from copy to process

--- Package.swift
+++ Package.swift
@@ -20,6 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ColorName",
-            dependencies: [], resources: [.copy("data.json")])
+            dependencies: [], resources: [.process("data.json")])
     ]
 )
